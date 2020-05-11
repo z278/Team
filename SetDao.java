280c5501b8cb54eff79a816c7341000579e2c54d
zcb2020 <64779218+zcb2020@users.noreply.github.com>
2020-05-11 21:39:32 +0800

    Create SetDao.java

---
+++ SetDao.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdao;
+
+import entity.UserPO;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import sessionFactory.HibernateSessionFactory;
+
+/**
+ @author b
+ */
+public class SetDao {
+    private Session session;
+    private Transaction transaction;
+    private Query query;
+    HibernateSessionFactory getSession;
+    public List queryInfo(String type, String value) {
+        getSession = new HibernateSessionFactory();
+        session=getSession.getSession();
+        try{
+            String hqlsql = "from UserPO as u where u.account_number=?";
+            query = session.createQuery(hqlsql);
+            query.setParameter(0,value);
+            List list = query.list();
+            transaction = session.beginTransaction();
+            transaction.commit();
+            return list;
+        }catch(Exception e){
+            message("LoginRegisterInfo类中有异常，异常为："+e);
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public boolean updateInfo(UserPO info){
+        try{
+            session = HibernateSessionFactory.getSession();
+            transaction = session.beginTransaction();
+            session.update(info);
+            transaction.commit();
+            session.close();
+            return true;
+        } catch(Exception e){
+            message("updateInfo.error:"+e);
+            e.printStackTrace();
+            return false;
+        }
+        
+    }
+    
+    public void message(String mess){
+        int type = JOptionPane.YES_NO_OPTION;
+        String title = "提示信息";
+        JOptionPane.showMessageDialog(null,mess,title,type);
+                
+    }
+}
